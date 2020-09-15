e52cc1ca32faa519411df9f7edd35af14dd1ffc3
Johannes <johannes.nichell@googlemail.com>
2020-09-15 22:20:47 +0200

    Operate on all layers in one go.

--- create_outline.py
+++ create_outline.py
@@ -4,41 +4,51 @@
 from gimpfu import *
 import math
 
-def create_outline(image, fillOpacity):
+def create_outline(image, fillOpacity, allLayers):
 
     xOffsets = [1, -1, 0, 0]
     yOffsets = [0, 0, 1, -1]
-    layers = []
 
-    originalLayer = image.active_layer
+    originalLayers = []
 
-    for i in range(4):
+    if(allLayers):
+        originalLayers = image.layers
+    else:
+        originalLayers.append(image.active_layer)
 
-        pdb.gimp_selection_none(image)
-        pdb.gimp_selection_all(image)
-        pdb.gimp_edit_copy(originalLayer)
 
-        selection = pdb.gimp_edit_paste(originalLayer, TRUE)
-        pdb.gimp_floating_sel_to_layer(selection)
+    for k in range(len(originalLayers)):
 
-        newLayer = image.active_layer
-        layers.append(newLayer)
+        layers = []
 
-        # pdb.gimp_selection_translate(image, xOffsets[i], yOffsets[i])
+        for i in range(4):
 
-        pdb.gimp_layer_translate (newLayer, xOffsets[i], yOffsets[i]);
-        pdb.gimp_selection_layer_alpha(newLayer)
+            pdb.gimp_selection_none(image)
+            pdb.gimp_image_set_active_layer(image, originalLayers[k])
+            pdb.gimp_selection_all(image)
+            pdb.gimp_edit_copy(originalLayers[k])
 
-        pdb.gimp_bucket_fill(newLayer, FG_BUCKET_FILL, NORMAL_MODE, fillOpacity, 0, TRUE, 0, 0)
+            selection = pdb.gimp_edit_paste(originalLayers[k], TRUE)
+            pdb.gimp_floating_sel_to_layer(selection)
 
-        # newLayer.translate(xOffsets[i], yOffsets[i])
+            newLayer = image.active_layer
+            layers.append(newLayer)
 
-    for i in range(len(layers) - 1):
-        j = (len(layers) - 2) - i
-        pdb.gimp_image_merge_down(image, layers[j], EXPAND_AS_NECESSARY)
+            # pdb.gimp_selection_translate(image, xOffsets[i], yOffsets[i])
 
-    pdb.gimp_image_lower_layer(image, image.active_layer)
-    image.active_layer.name = originalLayer.name + " Outline"
+            pdb.gimp_layer_translate (newLayer, xOffsets[i], yOffsets[i]);
+            pdb.gimp_selection_layer_alpha(newLayer)
+
+            pdb.gimp_bucket_fill(newLayer, FG_BUCKET_FILL, NORMAL_MODE, fillOpacity, 0, TRUE, 0, 0)
+
+            # newLayer.translate(xOffsets[i], yOffsets[i])
+
+        for i in range(len(layers) - 1):
+            j = (len(layers) - 2) - i
+            pdb.gimp_image_merge_down(image, layers[j], EXPAND_AS_NECESSARY)
+
+        pdb.gimp_image_lower_layer(image, image.active_layer)
+        image.active_layer.name = originalLayers[k].name + " Outline"
 
     pdb.gimp_selection_none(image)
 
@@ -53,7 +63,8 @@ register(
     "*",
     [
         (PF_IMAGE, 'image', 'Input image:', None),
-        (PF_FLOAT, 'fillOpacity', 'Fill opacity:', 100)
+        (PF_FLOAT, 'fillOpacity', 'Fill opacity:', 100),
+        (PF_BOOL, 'allLayers', 'All layers:', False)
     ],
     [],
     create_outline, menu="<Image>/Filters/Pixelart/")
